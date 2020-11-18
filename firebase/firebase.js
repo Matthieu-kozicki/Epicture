9b2e9970c21b943dbd62ff4934a887015d44e592
Yoan Saint Juste <yoan.saint-juste@epitech.eu>
2020-11-18 18:19:55 +0100

    [DEV][INIT] Init branch dev

---
+++ firebase/firebase.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCbvbigahv1ogH-kl9IoTexdWOkzlF4u_c",
+    authDomain: "dashboard-93.firebaseapp.com",
+    databaseURL: "https://dashboard-93.firebaseio.com",
+    projectId: "dashboard-93",
+    storageBucket: "dashboard-93.appspot.com",
+    messagingSenderId: "404943818447",
+    appId: "1:404943818447:web:b59d902fdfee4f0f50b1ba"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firebase();
+const auth = firebase.auth();
+
+export {
+    db, auth
+};
