1e0eb755ae1ef31684a6d89ff9173b2e72f63658
Kozicki Matthieu <matthieu.kozicki@epitech.eu>
2020-11-19 18:16:55 +0100

    [DEV][ADD] login with firebaseUI

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase/app'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCbvbigahv1ogH-kl9IoTexdWOkzlF4u_c",
+    authDomain: "dashboard-93.firebaseapp.com",
+    databaseURL: "https://dashboard-93.firebaseio.com",
+    projectId: "dashboard-93",
+    storageBucket: "dashboard-93.appspot.com",
+    messagingSenderId: "404943818447",
+    appId: "1:404943818447:web:b59d902fdfee4f0f50b1ba"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 createApp(App).use(router).mount('#app')
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,51 +1,43 @@
 <template>
-  <div class="login" id="mainDiv">
-    <h1>Dashboard du 93</h1>
-    <input id="loginInput" v-bind:value="login" placeholder="Email address" inputmode="email" />
-    <input id="loginInput" v-bind:value="password" placeholder="Password" type="password"/>
-    <button v-on:click="test" id="connectButton" type="button">
-      Connect
-    </button>
+  <div>
+    <h1>Login</h1>
+    <div id="firebaseui-auth-container"></div>
   </div>
 </template>
 
 <script>
+import { firebase } from '@firebase/app'
+import * as firebaseui from "firebaseui"
+import "firebaseui/dist/firebaseui.css";
+
+import '@firebase/auth'
+
 export default {
-  name: 'Login',
-  methods: {
-    test() {
-      console.log("weshh !!!");
-    }
-  },
-  data: () => {
+  data () {
     return {
-      login: "",
-      password: "",
+      email: '',
+      password: '',
+      error: ''
     }
   },
+  mounted() {
+        let ui = firebaseui.auth.AuthUI.getInstance();
+        if (!ui) {
+            ui = new firebaseui.auth.AuthUI(firebase.auth());
+        } 
+        var uiConfig = {
+            signInSuccessUrl: '#/HelloWorld', // This redirect can be achived by route using callback.
+            signInFlow: "popup",
+            signInOptions: [
+                firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID
+            ]
+        };
+        ui.start("#firebaseui-auth-container", uiConfig);
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-#loginInput {
-  color:blueviolet;
-  width: 200px;
-  height: 50px;
-  margin: 10px;
-  align-self: center;
-}
-
-#mainDiv {
-  display: flex;
-  flex-direction: column;
-}
-
-#connectButton {
-  width: auto;
-  height: 50px;
-  align-self: center;
-  margin: 10px;
-}
 </style>
